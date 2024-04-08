0d9724e6b8b25861636723e00e36b6308b5d0294
BasVanSch <93584219+BasVanSch@users.noreply.github.com>
2024-04-08 13:22:52 +0200

    Basis USER

--- WieFit/Common/Users/User.cs
+++ WieFit/Common/Users/User.cs
@@ -8,5 +8,25 @@ namespace WieFit.Common.Users
 {
     internal class User
     {
+        private readonly DAL.DAL database = DAL.DAL.Instance;
+
+        public string Username { get; private set; }
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public string Adress {  get; private set; }
+        public string PhoneNumber {  get; private set; }
+        public int Age { get; private set; }
+        public char Gender { get; private set; }
+
+        public User(string _username, string _name, string _email, string _adress, string _phoneNumber, int _age, char _gender)
+        {
+            Username = _username;
+            Name = _name;
+            Email = _email;
+            Adress = _adress;
+            PhoneNumber = _phoneNumber;
+            Age = _age;
+            Gender = _gender;
+        }
     }
 }
