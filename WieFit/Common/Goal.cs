09ca4c7ed0e67f8fc38abc43ca3e75462c5758c5
Noah Vranken <2303367vranken@zuyd.nl>
2024-04-08 13:38:09 +0200

    Organisator : User ctor method CreateActivity()

--- WieFit/Common/Goal.cs
+++ WieFit/Common/Goal.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Buffers.Text;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,17 @@ using System.Threading.Tasks;
 
 namespace WieFit.Common
 {
-    internal class Goal
+    internal class Organisator : User
     {
+
+        public Organisator(string _username, string _name, string _email, string _adress, string _phoneNumber, int _age, char _gender) : 
+            base (_username, _name, _email, _adress, _phoneNumber, _age, _gender)
+        {
+        }
+
+        public bool CreateActivity(Activity activity)
+        {
+            return database.CreateActivity(activity);
+        }
     }
 }
