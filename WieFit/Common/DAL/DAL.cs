c88af85aaa23a827a2dc742a032dcd6cb11141da
BasVanSch <93584219+BasVanSch@users.noreply.github.com>
2024-04-08 12:05:29 +0200

    Move DAL

---
+++ WieFit/Common/DAL/DAL.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WieFit.Common.Users;
+
+namespace WieFit.Common.DAL
+{
+    internal class DAL
+    {
+        private static readonly DAL instance = new DAL();
+        private readonly string connectionString = @"Data Source=.;Integrated Security=True;Encrypt=True;Trust Server Certificate=True;";
+
+        public static DAL Instance { get { return instance; } }
+
+        static DAL() { }
+        private DAL() { }
+
+        public bool CreateUser(User user)
+        {
+            try
+            {
+                using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+                {
+                    string userStatement = @"INSERT INTO USERS (username, name, email, password, adress, phonenumber, age, gender, type) VALUES ( @username, @name, @email, @password, @adress, @phonenumber, @age, @gender, @type );";
+
+                    sqlConnection.Open();
+
+                    using (SqlTransaction sqlTransaction = sqlConnection.BeginTransaction())
+                    {
+                        using (SqlCommand cmd = new SqlCommand(userStatement, sqlConnection, sqlTransaction))
+                        {
+                            cmd.Parameters.AddWithValue("@username", )
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
