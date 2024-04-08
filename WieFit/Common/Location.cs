c6ec6b01824bf052ce7dd8a912bd6320d8726fa7
kinggeert <geertbruls@outlook.com>
2024-04-08 13:19:53 +0200

    Added variables and constructors to Location.cs

--- WieFit/Common/Location.cs
+++ WieFit/Common/Location.cs
@@ -8,5 +8,21 @@ namespace WieFit.Common
 {
     internal class Location
     {
+        public int Id { get; set; }
+        public bool IsActive { get; set; }
+        public List<PlannedActivity> Activities { get; set; }
+
+        public Location(int id, bool isActive, List<PlannedActivity> activities)
+        {
+            Id = id;
+            IsActive = isActive;
+            Activities = activities;
+        }
+
+        public Location()
+        {
+            IsActive = false;
+            Activities = new List<PlannedActivity>();
+        }
     }
 }
