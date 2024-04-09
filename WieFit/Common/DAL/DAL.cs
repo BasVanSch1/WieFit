647c1870c04643bbd48b7025bb58daf7b8cac2ed
Noah Vranken <2303367vranken@zuyd.nl>
2024-04-09 10:36:17 +0200

    .

--- WieFit/Common/DAL/DAL.cs
+++ WieFit/Common/DAL/DAL.cs
@@ -84,5 +84,40 @@ namespace WieFit.Common.DAL
             }
             return true;
         }
+
+        public bool CreatePlanning(Planning planning)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    string userStatement = @"INSERT INTO PLANNING (isactive)  VALUES (@isactive)";
+
+                    connection.Open();
+
+                    using (SqlTransaction transaction = connection.BeginTransaction())
+                    {
+                        using (SqlCommand command = new SqlCommand(userStatement, connection, transaction))
+                        {
+                            command.Parameters.AddWithValue("@isactive", planning.IsActive);
+
+                            command.ExecuteNonQuery();
+
+                            //Get ID from database
+                            command.CommandText = "SELECT CAST(@@Identity as INT);";
+                            var id = (int)command.ExecuteScalar();
+                            planning.Id = id;
+
+                            transaction.Commit();
+                        }
+                    }
+                }
+            }
+            catch (Exception) // Catch all, nu tijdelijk geen error output. Als GUI wordt gemaakt zal er een pop-up komen met de error.
+            {
+                return false;
+            }
+            return true;
+        }
     }
 }
