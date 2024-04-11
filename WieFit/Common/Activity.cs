95eca1c5be15375163af71a6f3c7debcee189053
BasVanSch <93584219+BasVanSch@users.noreply.github.com>
2024-04-11 13:31:33 +0200

    fixes

--- WieFit/Common/Activity.cs
+++ WieFit/Common/Activity.cs
@@ -4,12 +4,13 @@ using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WieFit.Common.DAL;
 
 namespace WieFit.Common
 {
     internal class Activity
     {
-        static protected readonly DAL.DAL database = DAL.DAL.Instance;
+        private readonly ActivityDAL activityDAL = ActivityDAL.Instance;
         
         public int Id { get; set; }
         public string Name { get; set; }
@@ -28,7 +29,7 @@ namespace WieFit.Common
 
         public static Activity GetActivity(int id)
         {
-            return database.GetActivity(id);
+            return activityDAL.GetActivity(id);
         }
     }
     
