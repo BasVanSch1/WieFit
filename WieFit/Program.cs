8c2a67c2908ef6fa051fdd496e5f8a5211d89748
BasVanSch <93584219+BasVanSch@users.noreply.github.com>
2024-04-12 11:01:51 +0200

    Menu

--- WieFit/Program.cs
+++ WieFit/Program.cs
@@ -12,15 +12,11 @@ namespace WieFit
 
         static void Main(string[] args)
         {
-            //while (LoggedInUser == null)
-            //{
-            //    Login();
-            //}
-
-            //AddResult();
-            PlanActivity();
+            while (LoggedInUser == null)
+            {
+                Login();
+            }
         }
-
         static void CreateUser()
         {
             Console.Write("Enter your Username: ");
@@ -175,7 +171,7 @@ namespace WieFit
             {
                 loginManager = new LoginManager();
             }
-            bool loggedIn = false;
+            bool loggedIn = LoggedInUser == null;
 
             while (!loggedIn)
             {
@@ -384,6 +380,55 @@ namespace WieFit
             }
             else {
                 Console.WriteLine($"username: {c.Username}| name: {c.Name}| email: {c.Email}| address: {c.Adress}| phonenumber: {c.PhoneNumber}| age:{c.Age}| gender:{c.Gender}");
+            }
+        }
+        static void Logout()
+        {
+            if (LoggedInUser == null)
+            {
+                Console.WriteLine("There is no logged in user..");
+                return;
+            }
+        }
+        static void Menu()
+        {
+            bool inMenu = true;
+            string menuHeader =
+                @"
+                ====================================
+                 __          ___      ______ _ _   
+                 \ \        / (_)    |  ____(_) |  
+                  \ \  /\  / / _  ___| |__   _| |_ 
+                   \ \/  \/ / | |/ _ \  __| | | __|
+                    \  /\  /  | |  __/ |    | | |_ 
+                     \/  \/   |_|\___|_|    |_|\__|
+                ====================================";
+
+            Dictionary<int, KeyValuePair<string, Action>> menuItems = new()
+            {
+                [99] = new KeyValuePair<string, Action>("Logout", Logout),
+                [1] = new KeyValuePair<string, Action>("Get all locations", GetAllLocations),
+                [2] = new KeyValuePair<string, Action>("Add result", AddResult),
+            };
+
+            while (inMenu)
+            {
+                Console.Clear();
+                Console.Write(menuHeader);
+
+                foreach (KeyValuePair<int, KeyValuePair<string, Action>> pair in menuItems)
+                {
+                    Console.Write($"{pair.Key}) {pair.Value.Key}");
+                }
+
+                int choice = -1;
+                Console.Write("Enter a menu item number: ");
+                while (!Int32.TryParse(Console.ReadLine(), out choice))
+                {
+                    Console.WriteLine("Invalid input. Please enter an integer [0-99]");
+                    Console.Write("Enter a menu item number: ");
+                }
+
             }
         }
     }
